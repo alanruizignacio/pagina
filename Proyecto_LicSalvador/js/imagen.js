f4ea75a18e427d9ca1e97f6968c85c8b3fb82471
alanruizignacio <72716858+alanruizignacio@users.noreply.github.com>
2022-07-15 09:29:31 -0600

    Add files via upload

---
+++ Proyecto_LicSalvador/js/imagen.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+
+alert("Hola");
